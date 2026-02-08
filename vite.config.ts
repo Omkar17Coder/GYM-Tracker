7c0a021bda3cdb6aef4abfe07f2a2bff32262b38
Omkar Prashant Karmarkar <119780010+Omkar17Coder@users.noreply.github.com>
2026-02-08 23:19:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     base: "/GYM-Tracker/", // 🔴 REQUIRED for GitHub Pages
 
     server: {
-      port: 3000,
+      port: 8080,
       host: "0.0.0.0",
     },
 
